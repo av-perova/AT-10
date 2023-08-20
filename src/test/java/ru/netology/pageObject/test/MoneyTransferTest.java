6a983f0ea636bfa8956a2c87bd321c3b41cc41e0
Perova <av_perova@mail.ru>
2023-08-21 00:05:13 +0700

    Add first test

---
+++ src/test/java/ru/netology/pageObject/test/MoneyTransferTest.java
@@ -0,0 +1,42 @@
+package ru.netology.pageObject.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.pageObject.page.DashboardPage;
+import ru.netology.pageObject.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.pageObject.data.DataHelper.*;
+
+public class MoneyTransferTest {
+    DashboardPage dashboardPage;
+
+    @BeforeEach
+    void setup() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = getVerificationCodeFor(authInfo);
+        dashboardPage = verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    @DisplayName("Should Transfer From First To Second")
+    void shouldTransferFromFirstToSecond () {
+        var firstCardInfo = getFirstCardInfo();
+        var secondCardInfo = getSecondCardInfo();
+        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+        var amount = generateValidAmount(firstCardBalance);
+        var expectedBalanceFirstCard = firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardBalance + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        Assertions.assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        Assertions.assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+}
